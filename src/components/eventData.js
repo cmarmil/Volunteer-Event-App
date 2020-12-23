9fcf462b8c8204eba9fc36efd53f3c28c3282a6e
cmiller <cmiller@graphiclanguage.net>
2020-12-23 11:56:49 -0500

    event data source added

---
+++ src/components/eventData.js
@@ -0,0 +1,62 @@
+const events = [
+  {
+    name: "K-12 Computer Science Camp",
+    type: "Teaching",
+    startDate: "June 4, 2021",
+    endDate: "June 11, 2021",
+    description:
+      "Instructors needed to conduct coding and technology workshops to children ages K-12 in a summer camp setting.",
+    image:
+      "https://images.unsplash.com/photo-1585980243496-fe29a36bd382?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=818&q=80",
+  },
+  {
+    name: "Women's Web Development Meetup",
+    type: "Presentation",
+    startDate: "January 7, 2021",
+    endDate: "January 7, 2021",
+    description:
+      "Professional web developer needed to lead women's development meetup. Volunteer will provide a short presentation on a predetermined topic and answer audience questions as they arise.",
+    image:
+      "https://images.unsplash.com/photo-1551911729-286fd0896786?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=749&q=80",
+  },
+  {
+    name: "Coding for Greenville",
+    type: "Hackathon",
+    startDate: "March 12, 2021",
+    endDate: "March 14, 2021",
+    description:
+      "Join our annual weekend hackathon and use your programming skills to give back to the community. Volunteers will be organized into small groups and given a community focused problem to develop a solution for. Food and Drink will be provided.",
+    image:
+      "https://images.unsplash.com/photo-1559523182-a284c3fb7cff?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80",
+  },
+  {
+    name: "Crush Hunger Website",
+    type: "Project",
+    startDate: "February 1, 2021",
+    endDate: "February 28, 2021",
+    description:
+      "Help crush hunger using your web development skills! Crush Hunger is in need of a volunteer to develop a simple website to establish a web presence and provide information to the community. Volunteer must be proficient in Javascript, HTML, and CSS",
+    image:
+      "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80",
+  },
+  {
+    name: "Coding Instructor",
+    type: "Teaching",
+    startDate: "January 20, 2021",
+    endDate: "January 28, 2021",
+    description:
+      "Volunteers needed to teach small group of adults in a community sponsored development workshop. Volunteers must be proficient in Javascript, Python, or Ruby.",
+    image:
+      "https://images.unsplash.com/photo-1580982324076-d95230549339?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+  },
+  {
+    name: "Community Cafe Website Overhaul",
+    type: "Project",
+    startDate: "February 1, 2021",
+    endDate: "February 28, 2021",
+    description:
+      "Help the charity focused Community Cafe better serve our neighbors by providing them with an informational website. Volunteers must have Wordpress development experience.",
+    image:
+      "https://images.unsplash.com/photo-1554118811-1e0d58224f24?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=730&q=80",
+  },
+];
