6208929c9b2d25a70e6a0be97cf9665ae051de7f
cmiller <cmiller@graphiclanguage.net>
2020-12-23 23:33:47 -0500

    add tablet breakpoint for card deck

--- src/components/eventCardDeck.js
+++ src/components/eventCardDeck.js
@@ -8,6 +8,10 @@ const CardContainer = styled.div`
   grid-template-columns: repeat(3, 33%);
   grid-auto-rows: max-content;
   row-gap: 1em;
+  @media (max-width: 1024px) {
+    grid-template-columns: repeat(2, 50%);
+    grid-template-rows: 1fr;
+  }
   @media (max-width: 768px) {
     grid-template-columns: 1fr;
     grid-template-rows: 1fr;
