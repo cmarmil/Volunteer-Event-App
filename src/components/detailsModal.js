87099931685bb5a54cd8ec39d580ac5921f4c11b
cmiller <cmiller@graphiclanguage.net>
2020-12-26 11:57:31 -0500

    add success indicator to volunteer btuton

--- src/components/detailsModal.js
+++ src/components/detailsModal.js
@@ -1,21 +1,45 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { store } from "./appContext";
+import CheckmarkIcon from "./checkmarkIcon";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 
 const DetailsModal = () => {
   const { state, dispatch } = useContext(store);
+  const [isSuccess, setSuccess] = useState(false);
+  
   function toggleModal() {
     dispatch({ type: "TOGGLE_MODAL" });
   }
+
+  function volunteerClick() {
+    dispatch({ type: "ADD_EVENT", payload: state.activeEvent });
+    setSuccess(true);
+    setTimeout(() => {
+      toggleModal();
+      setSuccess(false);
+    }, 1000);
+  }
+
+  const SuccessButtonContent = (
+    <>
+      Success! <CheckmarkIcon />{" "}
+    </>
+  );
+
   return (
     <div>
       <Modal isOpen={state.modalOpen} toggle={toggleModal}>
-        <ModalHeader toggle={toggleModal}>{state.activeEvent?.name}</ModalHeader>
-        <ModalBody>
-          {state.activeEvent?.description}
-        </ModalBody>
+        <ModalHeader toggle={toggleModal}>
+          {state.activeEvent?.name}
+        </ModalHeader>
+        <ModalBody>{state.activeEvent?.description}</ModalBody>
         <ModalFooter>
-          <Button color="primary" onClick={() => dispatch({type: 'ADD_EVENT', payload: state.activeEvent})}>Volunteer</Button>{" "}
+          <Button
+            color={isSuccess ? "success" : "primary"}
+            onClick={volunteerClick}
+          >
+            {isSuccess ? SuccessButtonContent : "Volunteer"}
+          </Button>{" "}
           <Button color="secondary" onClick={toggleModal}>
             Cancel
           </Button>
